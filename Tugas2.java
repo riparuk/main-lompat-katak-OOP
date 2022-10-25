a21a279c0bc059fc3e8563e709508f2a637574cf
riparuk <faruqirifa@gmail.com>
2022-10-25 15:38:54 +0700

    10% Progress

---
+++ Tugas2.java
@@ -0,0 +1,48 @@
+class Koin{
+	private int nilai;
+	public Koin(){
+		
+	}
+	public Koin(int nilai){
+		
+	}
+	public int getNilai(){
+		return 0;
+	}
+	public void setNilai(int nilaibaru){
+		
+	}
+}
+class Monster{
+	private int nilai;
+	private String nama;
+	public Monster(){
+		
+	}
+	public Monster(int nilai, String nama){
+		
+	}
+	public int getNilai(){
+		return 0;
+	}
+}
+class Kotak{
+	private Koin coin;
+	private Monster monsta;
+	public Koin(){
+		
+	}
+	
+}
+class KotakPermainan{
+	
+}
+class Katak{
+	
+}
+class Pemain{
+	
+}
+class MainLompatKatak{
+	
+}
